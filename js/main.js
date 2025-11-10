48a599a78e95e6ea34a9ee9621c0e65e2a923013
christiantondo <christiantondo1@gmail.com>
2025-11-10 23:44:13 +0100

    sostituito il nome dell'id button in quanto andava in conflitto con bootstrap

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 console.log("JS ok")
 
 
-document.querySelector("#button").addEventListener("click", () => {
-    console.log("lampadina accesa");
-
+document.querySelector("#onoff").addEventListener("click", () => {
+    
     document.querySelector("#whitelamp").classList.toggle("lamp-visible");
     document.querySelector("#whitelamp").classList.toggle("lamp-invisible");
     document.querySelector("#yellowlamp").classList.toggle("lamp-invisible");
     document.querySelector("#yellowlamp").classList.toggle("lamp-visible");
-
+    
+    const buttoncontent = document.getElementById("onoff").innerHTML;
+    
+    if (buttoncontent == "Accendi") {
+        document.getElementById("onoff").innerHTML = "Spegni"
+    } else {
+        document.getElementById("onoff").innerHTML = "Accendi"
+    }
+    
 });
