79b718e8fa60128908702247623f8c7bd7f76ee7
christiantondo <christiantondo1@gmail.com>
2025-11-10 23:16:34 +0100

    aggiunta possibiltà di toggle

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ console.log("JS ok")
 document.querySelector("#button").addEventListener("click", () => {
     console.log("lampadina accesa");
 
-    document.querySelector("#whitelamp").classList.remove("lamp-visible");
-    document.querySelector("#whitelamp").classList.add("lamp-invisible");
-    document.querySelector("#yellowlamp").classList.remove("lamp-invisible");
-    document.querySelector("#yellowlamp").classList.add("lamp-visible");
+    document.querySelector("#whitelamp").classList.toggle("lamp-visible");
+    document.querySelector("#whitelamp").classList.toggle("lamp-invisible");
+    document.querySelector("#yellowlamp").classList.toggle("lamp-invisible");
+    document.querySelector("#yellowlamp").classList.toggle("lamp-visible");
 
 });
