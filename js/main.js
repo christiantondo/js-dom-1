396e67a1f2af707aee1c80eaf157ac6e92c5d250
christiantondo <christiantondo1@gmail.com>
2025-11-10 22:42:32 +0100

    sistemazione funzione

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ console.log("JS ok")
 
 
 document.querySelector("#button").addEventListener("click", () => {
-    console.log("cliccato");
+    console.log("lampadina accesa");
 
     document.querySelector("#whitelamp").classList.remove("lamp-visible");
     document.querySelector("#whitelamp").classList.add("lamp-invisible");
     document.querySelector("#yellowlamp").classList.remove("lamp-invisible");
-    document.querySelector("#yellowelamp").classList.add("lamp-visible");
-    
+    document.querySelector("#yellowlamp").classList.add("lamp-visible");
 
 });
