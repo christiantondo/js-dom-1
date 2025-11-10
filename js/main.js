94a123573bc9947cf3f9150225d11e37cfa27575
christiantondo <christiantondo1@gmail.com>
2025-11-10 22:25:30 +0100

    bozza funziona on/off

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("JS ok")
+
+
+document.querySelector("#button").addEventListener("click", () => {
+    console.log("cliccato");
+
+    document.querySelector("#whitelamp").classList.remove("lamp-visible");
+    document.querySelector("#whitelamp").classList.add("lamp-invisible");
+    document.querySelector("#yellowlamp").classList.remove("lamp-invisible");
+    document.querySelector("#yellowelamp").classList.add("lamp-visible");
+    
+
+});
